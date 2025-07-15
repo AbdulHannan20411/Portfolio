104c2b152703a95c8dedf5a13aeeab2da147b7c8
SSILHR\Ahannan <ahannan@ccrn.com>
2025-07-15 12:13:36 +0500

    file name updated

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,9 +4,10 @@ import { ProjectsSection } from "@/components/projects-section";
 import { ExperienceSection } from "@/components/experience-section";
 import { TechStackSection } from "@/components/tech-stack-section";
 import { Footer } from "@/components/footer";
-import { AboutSection } from "@/components/about-section";
 import { BlogSection } from "@/components/blog-section";
 import { ContactSection } from "@/components/contact-section";
+import { AboutSection } from "@/components/AboutSection";
+
 
 export default function Home() {
   return (
